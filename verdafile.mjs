219eeea084f790d042f961a81fb6d624305bf066
Heptazhou <heptazhou@icloud.com>
2021-06-30 17:08:01 +0000

    Update

--- verdafile.mjs
+++ verdafile.mjs
@@ -25,7 +25,7 @@ const IMAGE_TASKS = ".build/image-tasks";
 const GLYF_TTC = ".build/glyf-ttc";
 
 const DIST_TTC = "dist/.ttc";
-const DIST_SUPER_TTC = "dist/.super-ttc";
+const DIST_SUPER_TTC = "dist/@TTC";
 const ARCHIVE_DIR = "release-archives";
 
 const PATEL_C = ["node", "node_modules/patel/bin/patel-c"];
@@ -301,9 +301,7 @@ function makeFileName(prefix, suffix) {
 }
 function makeSuffix(w, wd, s, fallback) {
 	return (
-		(wd === WIDTH_NORMAL ? "" : wd) +
-			(w === WEIGHT_NORMAL ? "" : w) +
-			(s === SLOPE_NORMAL ? "" : s) || fallback
+		(wd === WIDTH_NORMAL ? "" : wd + "-") + w + (s === SLOPE_NORMAL ? "" : "-" + s) || fallback
 	);
 }
 
@@ -566,12 +564,7 @@ function fnStandardTtc(fIsGlyfTtc, prefix, suffixMapping, sfi) {
 //////               Font Collection                 //////
 ///////////////////////////////////////////////////////////
 
-const SpecificTtc = task.group(`ttc`, async (target, cgr) => {
-	const [cPlan] = await target.need(CollectPlans);
-	const ttcFiles = Array.from(Object.keys(cPlan[cgr].ttcComposition));
-	await target.need(ttcFiles.map(pt => CollectedTtcFile(cgr, pt)));
-});
-const SpecificSuperTtc = task.group(`super-ttc`, async (target, cgr) => {
+const SpecificSuperTtc = task.group(`ttc`, async (target, cgr) => {
 	await target.need(CollectedSuperTtcFile(cgr));
 });
 
@@ -910,6 +903,9 @@ const ChangeFileList = oracle.make(
 ///////////////////////////////////////////////////////////
 
 const Clean = task(`clean`, async () => {
+	build.deleteJournal();
+});
+const FullClean = task(`full-clean`, async () => {
 	await rm(BUILD);
 	await rm(DIST);
 	await rm(ARCHIVE_DIR);
@@ -1055,12 +1051,11 @@ function validateRecommendedWeight(w, value, label) {
 		extralight: 200,
 		light: 300,
 		regular: 400,
-		book: 450,
 		medium: 500,
 		semibold: 600,
 		bold: 700,
 		extrabold: 800,
-		heavy: 900
+		black: 900
 	};
 	if (RecommendedMenuWeights[w] && RecommendedMenuWeights[w] !== value) {
 		echo.warn(
