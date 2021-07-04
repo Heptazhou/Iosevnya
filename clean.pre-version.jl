e32d12befb7fd3a427bfe9cca7933454ce6e0dbd
Heptazhou <heptazhou@icloud.com>
2021-07-04 12:41:47 +0000

    Create

---
+++ clean.pre-version.jl
@@ -0,0 +1,49 @@
+function rmr(path::String)
+	ispath(path) ? (rm(path, recursive = true); 1) : 0
+end
+
+const list = [
+	# dir
+	".github"
+	"changes"
+	"images"
+	"sample-text"
+	"snapshot-src"
+	"utility"
+	# file
+	".editorconfig"
+	".eslintrc.json"
+	".gitattributes"
+	".npmrc"
+	".prettierrc.yaml"
+	"BackersArchive.md"
+	"build-plans.toml"
+	"LICENSE.md"
+	"README.md"
+	# path
+	"font-src/gen"
+	"font-src/glyphs"
+	"font-src/index.js"
+	"font-src/kits"
+	"font-src/meta/aesthetics.ptl"
+	"font-src/meta/macros.ptl"
+	"font-src/meta/unicode-knowledge.ptl"
+	"font-src/otl"
+	"font-src/support"
+	"params/ligation-set.toml"
+	"params/shape-slope.toml"
+	"params/shape-weight.toml"
+	"params/variants.toml"
+]
+
+try
+	occursin(r"iosevnya"i, splitdir(pwd())[2]) && throw("不允许在当前目录运行")
+	n = rmr.(list) |> sum
+	@info "完成 > $n"
+catch e
+	@info "错误"
+	@info e
+end
+print("> ")
+readline()
+
