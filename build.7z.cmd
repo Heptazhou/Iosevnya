ce3719e145c5ccd83147a5d1ec395465d1db89bb
Heptazhou <heptazhou@icloud.com>
2021-07-04 12:41:47 +0000

    Create

---
+++ build.7z.cmd
@@ -0,0 +1,16 @@
+@echo off
+chcp 65001>nul
+set VERSION=v0.0.0-0
+set NAME=iosevnya
+set ARGS=-m0=lzma -md3840m -mfb273 -mmt2 -ms -mx9 -stl
+cd dist
+del  %VERSION%*.7z /p 2>nul
+pause
+7z a %VERSION%.7z %ARGS% ./@TTC/%NAME%.ttc
+echo.
+7z a %VERSION%-ttf.7z %ARGS% ./%NAME%/ttf/%NAME%-*.ttf
+echo.
+7z a %VERSION%-ttf-unhinted.7z %ARGS% ./%NAME%/ttf-unhinted/%NAME%-*.ttf
+echo.
+echo.
+cmd
---
+++ clean.pre-build.jl
@@ -0,0 +1,39 @@
+function rmr(path::String)
+	ispath(path) ? (rm(path, recursive = true); 1) : 0
+end
+
+const list = [
+	# dir
+	".github"
+	"changes"
+	"images"
+	"sample-text"
+	"snapshot-src"
+	# file
+	".gitattributes"
+	".npmrc"
+	"BackersArchive.md"
+	"LICENSE.md"
+	"PACKAGE-LIST.md"
+	"package-lock.json"
+	# path
+	"utility/amend-readme"
+	"utility/ensure-verda-exists.js"
+	"utility/export-data"
+	"utility/generate-change-log"
+	"utility/generate-release-note"
+	"utility/generate-snapshot-page"
+	"utility/update-package-json-version"
+]
+
+try
+	splitdir(pwd())[2] |> lowercase == "iosevnya" || push!(list, "package.json", "README.md")
+	n = rmr.(list) |> sum
+	@info "完成 > $n"
+catch e
+	@info "错误"
+	@info e
+end
+print("> ")
+readline()
+
