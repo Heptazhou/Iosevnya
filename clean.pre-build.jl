d0f515c9f5a13c775079c8b85ce45029298ece6f
Heptazhou <heptazhou@icloud.com>
2021-07-04 12:41:47 +0000

    Create

---
+++ clean.pre-build.jl
@@ -0,0 +1,29 @@
+function rmr(path::String)
+	ispath(path) ? (rm(path, recursive = true); 1) : 0
+end
+
+const list = [
+	# dir
+	".github"
+	"changes"
+	"images"
+	"sample-text"
+	"snapshot-src"
+	# file
+	".gitattributes"
+	".npmrc"
+	"BackersArchive.md"
+	"LICENSE.md"
+]
+
+try
+	splitdir(pwd())[2] |> lowercase == "iosevnya" || push!(list, "README.md")
+	n = rmr.(list) |> sum
+	@info "完成 > $n"
+catch e
+	@info "错误"
+	@info e
+end
+print("> ")
+readline()
+
