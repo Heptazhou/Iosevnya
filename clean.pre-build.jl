4d85b16b59ba5e6d5509ae83eb7aa96da84c3aeb
Heptazhou <heptazhou@icloud.com>
2021-07-04 12:41:47 +0000

    Create

---
+++ clean.pre-build.jl
@@ -0,0 +1,36 @@
+function rmr(path::String)
+	ispath(path) ? (rm(path, recursive = true); 1) : 0
+end
+
+const list = [
+	# dir
+	".github"
+	"changes"
+	"doc"
+	"images"
+	"sample-text"
+	# file
+	"LICENSE.md"
+	"package-lock.json"
+	# path
+	"utility/amend-readme"
+	"utility/copy-char-name-to-markdown.mjs"
+	"utility/create-sha-file.mjs"
+	"utility/export-data"
+	"utility/generate-release-note"
+	"utility/generate-samples"
+	"utility/update-package-json-version"
+]
+
+try
+	occursin(r"iosevnya"i, splitdir(pwd())[2]) || push!(list, ".gitattributes", "package.json", "README.md")
+	n = rmr.(list) |> sum
+	@info "完成 > $n"
+catch e
+	@info "错误"
+	@info e
+end
+isempty(ARGS) || exit()
+print("> ")
+readline()
+
